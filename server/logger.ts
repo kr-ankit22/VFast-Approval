eed5865932332c682d83a9bbc1f8a0dff3fcad9b
Ankit Kumar <ind.ankit01@gmail.com>
2025-10-05 23:22:17 +0530

    fix: remove pino-pretty to ensure production stability

--- server/logger.ts
+++ server/logger.ts
@@ -1,26 +1,7 @@
 import pino from 'pino';
 
-// This is a more robust way to detect if the code is running on Netlify
-const isNetlify = process.env.NETLIFY === 'true';
-
-const logger = pino(
-  isNetlify
-    ? // In any Netlify environment (production, previews, etc.), use JSON logs.
-      {
-        level: 'info',
-      }
-    : // For local development, use pino-pretty.
-      {
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname',
-          },
-        },
-        level: 'info',
-      }
-);
+const logger = pino({
+  level: 'info',
+});
 
 export default logger;
